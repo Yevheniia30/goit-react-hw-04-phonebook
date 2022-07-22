0f5c0fbb82c17d31d7f8bb0778ff751cffc95d70
Yevheniia Suska <susskaya86@gmail.com>
2022-07-22 23:04:42 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
